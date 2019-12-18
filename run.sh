58fb62b8143348662560ba1f0569e7858a7c93a0
andredaa <post.for.andre@gmail.com>
2019-12-18 13:54:13 +0100

    Allow starting of multiple docker instances

--- run.sh
+++ run.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
-docker stop gracio_pystocs_instance
-docker rm gracio_pystocs_instance
 if [ "$#" -gt 0 ]; then # if command line arguments were given
-    docker run --name gracio_pystocs_instance -d gracio_pystocs --endpoint $1
+    docker stop gracio_pystocs_instance_$1
+    docker rm gracio_pystocs_instance_$1
+    docker run --name gracio_pystocs_instance_$1 -d gracio_pystocs --endpoint $1
+    # docker logs -f gracio_pystocs_instance_$1  ## do not force logs when multiple instances start
+
 else # no command line args -> don't choose endpoint
-    docker run --name gracio_pystocs_instance -d gracio_pystocs
+    docker stop gracio_pystocs_instance
+    docker rm gracio_pystocs_instance
+    docker run --name gracio_pystocs_instance -d gracio_pystocs --endpoint $1
+    docker logs -f gracio_pystocs_instance
 fi
-docker logs -f gracio_pystocs_instance
